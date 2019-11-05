17efe4c80a419a891f9ee496c28a7b033e95358e
Harry Gothold <harrygot@hotmail.com>
2019-11-05 23:57:31 +0000

    Created mongoose schemas, added middleware and created query and mutation

---
+++ resolvers.js
@@ -0,0 +1,25 @@
+// This file defines the actually action on the mutation/query
+exports.resolvers = {
+  Query: {
+    getAllRecipes: async (root, args, { Recipe }) => {
+      const allRecipes = await Recipe.find();
+      return allRecipes;
+    }
+  },
+  Mutation: {
+    addRecipe: async (
+      root,
+      { name, description, category, instructions, username },
+      { Recipe }
+    ) => {
+      const newRecipe = await new Recipe({
+        name,
+        description,
+        category,
+        instructions,
+        username
+      }).save();
+      return newRecipe;
+    }
+  }
+};
--- server.js
+++ server.js
@@ -1,14 +1,48 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config({ path: './variables.env' });
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv").config({ path: "./variables.env" });
+const Recipe = require("./models/Recipe");
+const User = require("./models/User");
 
-mongoose.connect(process.env.MONGO_URI).then(() => console.log('DB Connected')).catch(err => console.error(err));
+// Bring in graphql express middleware
+const { graphiqlExpress, graphqlExpress } = require("apollo-server-express");
+const { makeExecutableSchema } = require("graphql-tools");
+
+const { typeDefs } = require("./schema");
+const { resolvers } = require("./resolvers");
+
+// Create graphql schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB Connected"))
+  .catch(err => console.error(err));
 
 const app = express();
 
+// Create graphical application
+app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
+
+// Connect schemas with graphql
+app.use(
+  "/graphql",
+  bodyParser.json(),
+  graphqlExpress({
+    schema,
+    context: {
+      Recipe,
+      User
+    }
+  })
+);
+
 const PORT = process.env.PORT || 4444;
 
-
 app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`);
-})
+  console.log(`Server is running on ${PORT}`);
+});
