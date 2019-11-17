020b1427f5ec0a9e740abb97105d96f759362288
Harry Gothold <harrygot@hotmail.com>
2019-11-17 12:56:56 +0000

    Added update user recipe mutation

--- schema.js
+++ schema.js
@@ -49,5 +49,7 @@ type Mutation {
 
     likeRecipe(_id: ID!, username: String!): Recipe
     unlikeRecipe(_id: ID!, username: String!): Recipe
+
+    updateUserRecipe(_id: ID!, name: String!, imageUrl: String!, description: String!, category: String!): Recipe
 }
 `;
